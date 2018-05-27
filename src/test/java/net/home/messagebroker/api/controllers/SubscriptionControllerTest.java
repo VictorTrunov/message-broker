412f42770e096ab0efd6d22d131ae4984a2eb633
victor.trunov <victor.trunov@gmail.com>
2018-05-27 21:12:27 +0300

    Start controllers tests

---
+++ src/test/java/net/home/messagebroker/api/controllers/SubscriptionControllerTest.java
@@ -0,0 +1,7 @@
+package net.home.messagebroker.api.controllers;
+
+import static org.junit.Assert.*;
+
+public class SubscriptionControllerTest {
+
+}
---
+++ src/test/java/net/home/messagebroker/api/controllers/TopicControllerTest.java
@@ -0,0 +1,55 @@
+package net.home.messagebroker.api.controllers;
+
+import net.home.messagebroker.MessageBrokerApplication;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MessageBrokerApplication.class)
+@WebAppConfiguration
+public class TopicControllerTest {
+
+    private MediaType mediaType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+}
